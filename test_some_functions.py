2ca1725d5e9fe400f9adde0d5ff09f2bb2007f01
leandro <leandro@ebi.ac.uk>
2019-05-03 13:37:59 +0100

    Testing get_interval_seqs

---
+++ test_some_functions.py
@@ -0,0 +1,16 @@
+from make_prg_from_msa import  *
+from Bio import AlignIO
+import sys
+
+def test_get_interval_seqs(inputFile):
+    print(f"Testing {inputFile}")
+    seqs = AlignIO.read(inputFile, "fasta")
+    print(f"Input:{seqs}\nOutput:{get_interval_seqs(seqs)}")
+    print()
+
+
+for i in range(5):
+    try:
+        test_get_interval_seqs(f"test/get_interval_seqs_test{i}.fa")
+    except:
+        print(sys.exc_info()[0])
